90891892b5e7e50e755d3da98c25c03e5094dad7
Light <kberle11@student.aau.dk>
2017-07-21 14:17:11 +0200

    Added Chunk Manager and World Manager

---
+++ Summoner/Assets/Scripts/Systems/Landscape Generation/WorldManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldManager : MonoBehaviour {
+
+	public static WorldManager instance;
+	public static Generator generator;
+
+	public int x;
+	public int y;
+	public int z;
+
+	public GameObject chunkManagerPrefab;
+
+	public string seed = "Testing";
+	public bool useRandomSeed = false;
+
+	[Range(0,100)]
+	public int randomFill;
+	[Range(0,100)]
+	public int randomAdd;
+
+	void Awake(){
+		Random.InitState (seed.GetHashCode());
+		generator = new Generator();
+
+		if(instance){
+			Destroy(this);
+		}else{
+			instance = this;
+		}
+
+		for(int i = -(y/2); i < y/2; i++){
+			for(int j = -(z/2); j < z/2; j++){
+				for(int k = -(x/2); k < x/2; k++){
+					GameObject go = Instantiate(chunkManagerPrefab, new Vector3(16*k, 16*i, 16*j), Quaternion.identity);
+					go.GetComponent<ChunkManager>().Init(go,randomFill, randomAdd, generator);
+				}
+			}
+		}
+
+		//chunkManagerPrefab = Resources.Load("ChunkManager") as GameObject;
+	}
+}
