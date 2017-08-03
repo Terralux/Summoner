36f5919a0239fbba604c8cd8deeface9caa6a609
Patrick Johansen <mrrichtofen@gmail.com>
2017-08-03 13:09:26 +0200

    Added inventory again with support for item type casting to resource etc

--- Summoner/Assets/Scripts/Systems/Game Mechanics/Inventory.cs
+++ Summoner/Assets/Scripts/Systems/Game Mechanics/Inventory.cs
@@ -5,15 +5,46 @@ using UnityEngine;
 public class Inventory {
 
 	[Range(0f,40f)]
-	public int capacity;
-	public List<BaseItem> items;
+	public int maxSlots;
+	private int usedSlots;
+	private Dictionary<BaseItem, int> items;
 
-	public void AddItem(BaseItem item) {
-		items.Add (item);
+	public Inventory() {
+		items = new Dictionary<BaseItem, int> ();
+	}
+
+	public void AddItem(BaseItem item, int quantity) {
+		if (item as Resource == null && item as Decoration == null) {
+			items.Add (item, 1);
+			usedSlots++;
+		} else {
+			if (ExistsInInventory (item)) {
+				items [item] += quantity;
+			} else {
+				items.Add (item, quantity);
+				usedSlots++;
+			}
+		}
 	}
 
 	public void RemoveItem(BaseItem item) {
-		items.Remove (item);
+		if (ExistsInInventory (item)) {
+			items.Remove (item);
+			usedSlots--;
+		}
+	}
+
+	public bool ExistsInInventory(BaseItem item) {
+		return items.ContainsKey (item);
+	}
+
+	public void AdjustQuantity(BaseItem item, int value) {
+		if (ExistsInInventory (item)) {
+			items [item] += value;
+			if (items [item] == 0) {
+				RemoveItem (item);
+			}
+		}
 	}
 
 }
