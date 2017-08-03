7f5fa2a1db8920e6905ee0276574cf879de9534e
Patrick Johansen <mrrichtofen@gmail.com>
2017-08-03 11:29:44 +0200

    Made changes to inventory to use dictionary instead with quantity functionality and ExistsInInventory checks

--- Summoner/Assets/Scripts/Systems/Game Mechanics/Inventory.cs
+++ Summoner/Assets/Scripts/Systems/Game Mechanics/Inventory.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 
 public class Inventory {
 
-	[Range(0f,40f)]
-	public int capacity;
-	public List<BaseItem> items;
+	[Range(0f,100f)]
+	public int maxSlots;
+	private int slotsUsed;
+	Dictionary<BaseItem, int> items = new Dictionary<BaseItem, int>();
 
 	public void AddItem(BaseItem item) {
-		items.Add (item);
+		if (!ExistsInInventory (item)) {
+			items.Add (item, 1);
+			AdjustSlotsUsed (1);
+		} 
 	}
 
 	public void RemoveItem(BaseItem item) {
-		items.Remove (item);
+		if (ExistsInInventory (item)) {
+			items.Remove (item);
+			AdjustSlotsUsed (-1);
+		}
+	}
+
+	public void AdjustQuantity(BaseItem item, int value) {
+		if (ExistsInInventory (item)) {
+			items [item] += value;
+		}
+	}
+
+	public bool ExistsInInventory(BaseItem item) {
+		if (items.ContainsKey (item)) {
+			return true;
+		}
+		return false;
+	}
+
+	private void AdjustSlotsUsed(int value) {
+		slotsUsed += value;
 	}
 
 }
