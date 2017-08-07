1d7793fa681658a3077b10ab4312d9745caeef4a
FenrirGameStudio <account@fenrirgamestudio.dk>
2017-08-07 11:13:45 +0200

    Added a placeholder building prefab and started work on structure interaction

---
+++ Summoner/Assets/Scripts/Systems/Items/Structures/AddStructure.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddStructure : MonoBehaviour {
+
+	public GameObject structure; 
+
+	public void Update(){
+		if (Input.GetKeyDown (KeyCode.B)) {
+			PlaceStructure ();
+		}
+	}
+
+	public void PlaceStructure(){
+		/*MeshCollider mesh = new MeshCollider();
+		if (mesh.GetComponent<MeshFilter> ().sharedMesh.vertices.Length > 0) {
+			Instantiate (structure, Random.onUnitSphere, Quaternion.identity);
+		}
+		if (mesh.GetComponent<MeshFilter> ().sharedMesh != null) {
+			Instantiate (structure, Random.onUnitSphere, Quaternion.identity);
+		}*/
+		Instantiate (structure, transform.position, Quaternion.identity);
+	}
+}
--- Summoner/Assets/Scripts/Systems/Items/Structures/Structure.cs
+++ Summoner/Assets/Scripts/Systems/Items/Structures/Structure.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class Structure : Placable {
 
+	public GameObject structure; 
 
+	public void update(){
+		if (Input.GetKey (KeyCode.B)) {
+			PlaceStructure ();
+		}
+	}
+
+	public void PlaceStructure(){
+		MeshCollider mesh = new MeshCollider();
+		if (mesh.GetComponent<MeshFilter> ().sharedMesh.vertices.Length > 0) {
+			Instantiate (structure, Random.onUnitSphere, Quaternion.identity);
+		}
+	}
 }
