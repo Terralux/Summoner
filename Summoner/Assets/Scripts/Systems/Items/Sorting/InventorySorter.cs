5e855dfc969a5e60ee91be4a05ab6cf3833fcd37
Patrick Johansen <mrrichtofen@gmail.com>
2017-08-09 14:27:40 +0200

    Implemented ability to sort inventory based on item types: P, U, W, R

--- Summoner/Assets/Scripts/Systems/Items/Sorting/InventorySorter.cs
+++ Summoner/Assets/Scripts/Systems/Items/Sorting/InventorySorter.cs
@@ -6,22 +6,44 @@ public class InventorySorter
 {
 
 	// Types: Placable, Utility, Weapon, Resource
+	// IMPORTANT: If class names change for items, refactoring here is necessary!
+
+	private string orderByType;
+
 
 	// Sort Inventory by type in the order: Placables, Utilities, Weapons, Resources
-	public void SortByType (List<InventorySlot> inventory)
+	public List<InventorySlot> SortByType (List<InventorySlot> inventory, string type = "Placable", bool useStandardSort = true)
 	{
-		// Sorts into type order: P, R, U, W
+		// Sorts into type order: R, P, U, W
 		inventory.Sort (
 			delegate(InventorySlot i1, InventorySlot i2) {
-				return i1.item.GetType().FullName.CompareTo(i2.item.GetType().FullName);
+
+				if (IsUsableOrPlacable (i1) && IsUsableOrPlacable (i2)) {
+					// Compare parent class names for both
+					return i1.item.GetType ().BaseType.FullName.CompareTo (i2.item.GetType ().BaseType.FullName);
+				} else if (IsUsableOrPlacable (i1)) {
+					// Compare parent class name of i1 with current class name of i2
+					return i1.item.GetType ().BaseType.FullName.CompareTo (i2.item.GetType ().FullName);
+				} else if (IsUsableOrPlacable (i2)) {
+					// Compare current class name of i1 with parent class name of i2
+					return i1.item.GetType ().FullName.CompareTo (i2.item.GetType ().BaseType.FullName);
+				} else {
+					// Compare current class names for both
+					return i1.item.GetType ().FullName.CompareTo (i2.item.GetType ().FullName);
+				}
 			}	
 		); 
 
-		// Swap all [R <-> U,    W <-> R] items
-		SwapItemsByTypeOrder(inventory);
-		Debug.Log("Done sorting items in type order: P, U, W, R");
+
+		if (useStandardSort) {
+			return SortItemsByStandardOrder (inventory);
+		} else {
+			return SortItemsBySelectOrder (inventory, type);
+		}
+
 	}
-	// Sort Inventory A-Z
+
+	// Sort items not slots in Inventory A-Z
 	public void SortAlphabetically (List<InventorySlot> inventory)
 	{
 		inventory.Sort (
@@ -29,28 +51,43 @@ public class InventorySorter
 				return i1.item.itemName.CompareTo (i2.item.itemName); 
 			});
 	}
+	// Moves all resources (slots) in inventory last
+	private List<InventorySlot> SortItemsByStandardOrder (List<InventorySlot> inventory)
+	{
+		List<InventorySlot> resourcesToMove = inventory.FindAll (IsResourceAndNotPlacable);
+		inventory.RemoveAll (IsResourceAndNotPlacable);
+		inventory.AddRange (resourcesToMove);
+		return inventory;
+	}
 
-	// Sort Placables into Decorations & Structures, Weapons into Melee & Ranged, Usables into Utility & Vehicles
-	private void SubTypeSort ()
+	private List<InventorySlot> SortItemsBySelectOrder (List<InventorySlot> inventory, string type = "Placable")
+	{
+		orderByType = type;
+		List<InventorySlot> resourcesToMove = inventory.FindAll (IsSelectType);
+		inventory.RemoveAll (IsSelectType);
+		inventory.AddRange (resourcesToMove);
+		return inventory;
+	}
+
+	private bool IsSelectType (InventorySlot slot)
 	{
 		
+		if (IsUsableOrPlacable (slot)) {
+			return slot.item.GetType ().BaseType.FullName != orderByType;
+		} else {
+			return slot.item.GetType ().FullName != orderByType;
+		} 
+
 	}
 
-	private void SwapItemsByTypeOrder(List<InventorySlot> inventory) {
-		List<InventorySlot> weaponsToMove = inventory.FindAll(IsWeapon);
-		inventory.RemoveAll (IsWeapon);
-		inventory.AddRange (weaponsToMove);
-		List<InventorySlot> resourcesToMove = inventory.FindAll(IsResource);
-		inventory.RemoveAll (IsResource);
-		inventory.AddRange (resourcesToMove);
+	private bool IsResourceAndNotPlacable (InventorySlot slot)
+	{
+		return ((slot.item is Resource) && !(slot.item is Placable));
 	}
 
-	private bool IsResource(InventorySlot slot) {
-		return slot is Resource;
-	}
-
-	private bool IsWeapon(InventorySlot slot) {
-		return slot is Weapon;
+	private bool IsUsableOrPlacable (InventorySlot slot)
+	{
+		return (slot.item is Useables || slot.item is Placable);
 	}
 
 }
