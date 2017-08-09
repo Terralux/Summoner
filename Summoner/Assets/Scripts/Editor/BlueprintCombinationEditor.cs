d5c38a64c3ca787bf225bdc1e0a8ee38b044547a
Light <kberle11@student.aau.dk>
2017-08-09 17:59:19 +0200

    Sorted out the blueprint system

---
+++ Summoner/Assets/Scripts/Editor/BlueprintCombinationEditor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BlueprintCombinationEditor : EditorWindow {
+
+	public string myString;
+	bool myBool;
+	float myFloat;
+
+	private static InventionCollection inventions;
+
+	private Blueprint ingredient1;
+	private Blueprint ingredient2;
+	private Blueprint ingredient3;
+
+	[MenuItem("Window/Trailblazer/Blueprint Combination Editor")]
+	static void Init(){
+		BlueprintCombinationEditor window = (BlueprintCombinationEditor)EditorWindow.GetWindow(typeof(BlueprintCombinationEditor));
+		inventions = (InventionCollection) AssetDatabase.LoadAssetAtPath("Assets/ScriptableObjects/BlueprintCombinations.asset", typeof(InventionCollection));
+		window.Show();
+	}
+
+	void OnGUI(){
+		if(inventions == null){
+			return;
+		}
+
+		/*
+		 * option to add material 1, 2 and 3
+		 * option to add a result
+		*/
+
+		GUILayout.Label("Blueprint Combination", EditorStyles.boldLabel);
+
+		ingredient1 = (Blueprint) EditorGUI.ObjectField(new Rect(3, 3, position.width - 6, 20), "Find Dependency", ingredient1, typeof(Blueprint));
+
+	}
+
+}
---
+++ Summoner/Assets/Scripts/Systems/Game Mechanics/InventionCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Invention Collection", menuName = "Trailblazer/Blueprints", order = 0)]
+public class InventionCollection : ScriptableObject {
+	[SerializeField]
+	public List<Invention> inventions = new List<Invention>();
+}
