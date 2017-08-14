134735dce6beb1f6b47c6e0cb9d3b1f0906d1a1e
FenrirGameStudio <account@fenrirgamestudio.dk>
2017-08-14 17:54:54 +0200

    Added framework for enemy AI with different states

---
+++ Summoner/Assets/Scripts/Systems/Gameplay/Characters/Enemies/AI/States/PassiveState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassiveState : State{
+
+	public bool idle = true;
+
+	void Update(){
+		if (idle) {
+			StartCoroutine ("Walk");
+			idle = false;
+		}
+		if (!idle) {
+			StartCoroutine ("Idle");
+			idle = true;
+		}
+	}
+
+	IEnumerator Walk(){
+		yield return new WaitForSeconds (6);
+		anim.SetFloat ("Speed", 1);
+
+
+	}
+
+	IEnumerator Idle(){
+		yield return new WaitForSeconds (5);
+		anim.SetFloat ("Speed", 0.1f);
+	}
+}
---
+++ Summoner/Assets/Scripts/Systems/Gameplay/Characters/Enemies/AI/States/State.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : MonoBehaviour {
+
+	public EnemyStats stats;
+	public Rigidbody rb;
+	public Animator anim;
+	public List<State> states;
+
+	void Awake(){
+		rb = GetComponent<Rigidbody>();
+		anim = GetComponent<Animator>();
+	}
+}
