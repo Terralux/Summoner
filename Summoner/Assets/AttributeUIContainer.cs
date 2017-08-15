a1ff5c7adbd374f7c0670ce4a021c8de37bb9b5b
Patrick Johansen <mrrichtofen@gmail.com>
2017-08-15 15:35:27 +0200

    Finished implementing inventory UI first version. Graphics will need to be changed and possibly the structure. Functionality is operational.

---
+++ Summoner/Assets/AttributeUIContainer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttributeUIContainer : MonoBehaviour {
+
+	public Text attValue;
+	public Text attImage;
+	private ElementalAffinity myEA;
+
+	public void Init (ElementalAffinity ea) {
+		myEA = ea;
+		UpdateGraphics ();
+	}
+
+	public void UpdateGraphics(){
+		attImage.text = myEA.targetElement.ToString ();
+		attValue.text = myEA.value.ToString ();
+	}
+}
