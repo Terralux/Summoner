a913900d26c4e79146515c05fdb5dd6594cab480
Patrick Johansen <mrrichtofen@gmail.com>
2017-10-11 11:48:07 +0200

    Made framework for structure functionality and well of life.

---
+++ Summoner/Assets/Scripts/Systems/Structures/WellOfLife.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WellOfLife : MonoBehaviour {
+
+	public CitySetup citySetup;
+	public float radius;
+
+	// Use this for initialization
+	void Awake () {
+		citySetup = new CitySetup(transform.position, radius);
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.DrawSphere (transform.position, radius);
+	}
+}
