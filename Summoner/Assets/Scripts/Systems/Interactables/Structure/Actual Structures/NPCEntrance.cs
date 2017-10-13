3eb5a06af706951cc1784d192e0aec6d6bd76def
Patrick Johansen <mrrichtofen@gmail.com>
2017-10-13 13:13:45 +0200

    Fixed errors

---
+++ Summoner/Assets/Scripts/Systems/Interactables/Structure/Actual Structures/NPCEntrance.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCEntrance : BaseStructure {
+	#region implemented abstract members of Interactive
+
+	public float npcArrivalRate;
+	public List<Citizen> pendingImmigrants = new List<Citizen> ();
+
+	public override void OnInteract ()
+	{
+		Debug.Log ("No new immigrants. This building does not have an interaction :D");
+	}
+
+	#endregion
+
+	void Awake() {
+		if (isPowered) {
+			InvokeRepeating ("NewArrival", npcArrivalRate, npcArrivalRate);
+		}
+	}
+
+	void NewArrival() {
+		pendingImmigrants.Add (new Citizen ());
+	}
+
+	void Shutdown(){
+		CancelInvoke ();
+		OnShutdown (this);
+	}
+}
---
+++ Summoner/Assets/Scripts/Systems/Interactables/Structure/Framework/CitySetup.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitySetup {
+
+	Vector3 cityCenter;
+	float radius;
+
+	public List<BaseStructure> structuresWithinReach;
+
+	public CitySetup(Vector3 cityCenter, float radius) {
+		this.cityCenter = cityCenter;
+		this.radius = radius;
+
+		structuresWithinReach = new List<BaseStructure> ();
+		UpdateStructureStates ();
+	}
+
+	public void AdjustStructureRegisterRange(float range){
+		radius = range;
+		UpdateStructureStates ();
+	}
+
+	void StructuresInsideCircle() {
+		Collider[] hitColliders = Physics.OverlapSphere(cityCenter, radius);
+		foreach (Collider col in hitColliders) {
+			BaseStructure structure = col.gameObject.GetComponent<BaseStructure> ();
+			if (structure != null) {
+				structuresWithinReach.Add (structure);
+			}
+		}
+	}
+
+	void PowerReachableStructures() {
+		foreach (BaseStructure structure in structuresWithinReach) {
+			structure.isPowered = true;
+		}
+	}
+
+	public void UpdateStructureStates() {
+		foreach (BaseStructure structure in structuresWithinReach) {
+			structure.isPowered = false;
+		}
+		structuresWithinReach.Clear ();
+		StructuresInsideCircle ();
+		PowerReachableStructures ();
+	}
+
+	public void RegisterStructure(BaseStructure structure){
+		structure.OnShutdown += RemoveStructure;
+	}
+
+	public void RemoveStructure(BaseStructure structure){
+		structuresWithinReach.Remove (structure);
+	}
+
+}
