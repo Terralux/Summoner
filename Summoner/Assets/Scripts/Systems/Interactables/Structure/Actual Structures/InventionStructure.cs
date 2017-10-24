7cd976e3249ab26faececc5c7e3a3554f7f2d8f4
Patrick Johansen <mrrichtofen@gmail.com>
2017-10-24 14:57:43 +0200

    Added methods to invention class to support comparison and created inventionstructure that works like a workbench (minecraft) to build new structures using discovered blueprints

---
+++ Summoner/Assets/Scripts/Systems/Interactables/Structure/Actual Structures/InventionStructure.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class InventionStructure : InteractiveStructure
+{
+
+	// Public fields
+	public int numberOfItemsNeeded = 3;
+
+	// Private fields
+	HashSet<Invention> inventionsDiscovered = new HashSet<Invention> ();
+	InventionCollection inventionCollection = (InventionCollection)AssetDatabase.LoadAssetAtPath ("Assets/ScriptableObjects/BlueprintCombinations.asset", typeof(InventionCollection));
+
+	#region implemented abstract members of Interactive
+
+	public override void OnInteract () {
+		// Toggle UI for combining blue prints
+
+	}
+
+	#endregion
+
+	/// <summary>
+	/// Method to find an invention that matches the combination of blueprints built by the player using appropriate UI. Call the method on submit
+	/// </summary>
+	/// <returns>Invention. If no match returns null</returns>
+	/// <param name="blueprintPattern">Blueprint pattern.</param>
+	public Invention FindMatch(HashSet<Blueprint> blueprintPattern) {
+		foreach (Invention invention in inventionCollection.inventions) {
+			if (invention.FindMatch (blueprintPattern)) {
+				return invention;
+			}
+		}
+		return null;
+	}
+
+}
