18954dd658240836140b092e3672528d064d3d4d
Patrick Johansen <mrrichtofen@gmail.com>
2017-10-25 14:56:09 +0200

    Moved scripts around and created framework for vehices and utilities

--- Summoner/Assets/Scripts/Systems/Items/Utility/Vehicle.cs
+++ Summoner/Assets/Scripts/Systems/Items/Utility/Vehicle.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 [System.Serializable, CreateAssetMenu(fileName="New Vehicle", menuName = "Trailblazer/Items/Vehicle", order=4)]
 public class Vehicle : Useables {
-	/*
-	 * Once a Vehicle is triggered, the character will mount that vehicle and the controller of the character will change
-	*/
+	#region implemented abstract members of Useables
+
+	public override void OnActivateFromMenu ()
+	{
+		// Choose one: Attach a script to player or instantiate prefab (prefab has behavior)
+	}
+
+	#endregion
+
+
 }
